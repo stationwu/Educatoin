ef981b1c8048b441701f1dcb5e5a3d682b616ae8
Arthur Wu <arthur.wu@sap.com>
2017-09-26 16:41:23 +0800

    initial commit

---
+++ src/main/java/com/edu/controller/WechatController.java
@@ -0,0 +1,108 @@
+package com.edu.controller;
+
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Binary Wang(https://github.com/binarywang)
+ */
+@RestController
+@RequestMapping("/wechat/portal")
+public class WechatController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private WxMpService wxService;
+
+    @Autowired
+    private WxMpMessageRouter router;
+
+    @GetMapping(produces = "text/plain;charset=utf-8")
+    public String authGet(
+            @RequestParam(name = "signature",
+                    required = false) String signature,
+            @RequestParam(name = "timestamp",
+                    required = false) String timestamp,
+            @RequestParam(name = "nonce", required = false) String nonce,
+            @RequestParam(name = "echostr", required = false) String echostr) {
+
+        this.logger.info("\n received verification message：[{}, {}, {}, {}]", signature,
+                timestamp, nonce, echostr);
+
+        if (StringUtils.isAnyBlank(signature, timestamp, nonce, echostr)) {
+            throw new IllegalArgumentException("illegal parameter");
+        }
+
+        if (this.wxService.checkSignature(timestamp, nonce, signature)) {
+            return echostr;
+        }
+
+        return "invaild request";
+    }
+
+    @PostMapping(produces = "application/xml; charset=UTF-8")
+    public String post(@RequestBody String requestBody,
+                       @RequestParam("signature") String signature,
+                       @RequestParam("timestamp") String timestamp,
+                       @RequestParam("nonce") String nonce,
+                       @RequestParam(name = "encrypt_type",
+                               required = false) String encType,
+                       @RequestParam(name = "msg_signature",
+                               required = false) String msgSignature) {
+        this.logger.info(
+                "\n received message：[signature=[{}], encType=[{}], msgSignature=[{}],"
+                        + " timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
+                signature, encType, msgSignature, timestamp, nonce, requestBody);
+
+        if (!this.wxService.checkSignature(timestamp, nonce, signature)) {
+            throw new IllegalArgumentException("illegal parameter，fake request!");
+        }
+
+        String out = null;
+        if (encType == null) {
+            // 明文传输的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(requestBody);
+            WxMpXmlOutMessage outMessage = this.route(inMessage);
+            if (outMessage == null) {
+                return "";
+            }
+
+            out = outMessage.toXml();
+        } else if ("aes".equals(encType)) {
+            // aes加密的消息
+            WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(
+                    requestBody, this.wxService.getWxMpConfigStorage(), timestamp,
+                    nonce, msgSignature);
+            this.logger.debug("\nDecrypted content：\n{} ", inMessage.toString());
+            WxMpXmlOutMessage outMessage = this.route(inMessage);
+            if (outMessage == null) {
+                return "";
+            }
+
+            out = outMessage
+                    .toEncryptedXml(this.wxService.getWxMpConfigStorage());
+        }
+
+        this.logger.debug("\n package message：{}", out);
+
+        return out;
+    }
+
+    private WxMpXmlOutMessage route(WxMpXmlMessage message) {
+        try {
+            return this.router.route(message);
+        } catch (Exception e) {
+            this.logger.error(e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+}
