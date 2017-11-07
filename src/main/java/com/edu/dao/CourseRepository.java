4882dd8af3ff6da784c17cc74e59d1bbcef76554
Arthur Wu <arthur.wu@sap.com>
2017-11-07 18:26:00 +0800

    add course repository

--- src/main/java/com/edu/dao/CourseRepository.java
+++ src/main/java/com/edu/dao/CourseRepository.java
@@ -2,9 +2,11 @@ package com.edu.dao;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.edu.domain.Course;
@@ -12,4 +14,6 @@ import com.edu.domain.Course;
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
 	Page<Course> findAllOrderByCourseName(Pageable pageable);
 	Collection<Course> findByDateOrderByTimeFromAsc(String date);
+	@Query("select c from Course c where c.date < ?1 and c.timeFrom < ?2 order by c.date desc, c.timeFrom")
+	 List<?> search(String data, String hour, Pageable pageable);
 }
