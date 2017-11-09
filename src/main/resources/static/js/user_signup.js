6e1217d1f097683f095cdce18e2f47cf56318dab
Guo, Yong Kang <guogin@outlook.com>
2017-11-09 18:14:59 +0800

    Use Vue.js

--- src/main/resources/static/js/user_signup.js
+++ src/main/resources/static/js/user_signup.js
@@ -1,41 +1,16 @@
-$(document).ready(function(){
-  $("#btnAdd").click(function(){
-    console.log("#btnAdd clicked");
-
-    var count = $('#children div.weui-panel__bd').length;
-    count++;
-
-    var nodeId = "child-" + count;
-    $('#children div.weui-panel__bd:last').after(
-        '<div id="'+nodeId+'" class="weui-panel__bd" >' +
-           '<div class="weui-cells">' +
-            '<div class="weui-cell">' +
-                '<div class="weui-cell__bd">' +
-                    '<input class="weui-input" placeholder="孩子姓名" type="text" />' +
-                '</div>'+
-            '</div>'+
-        '</div>'+
-        '<div class="weui-cell">'+
-            '<div class="weui-cell__hd">'+
-                '<label class="weui-label" for="">'+
-                    '生日'+
-                '</label>'+
-            '</div>'+
-            '<div class="weui-cell__bd">'+
-                '<input class="weui-input" type="date" value=""/>'+
-            '</div>'+
-        '</div>'+
-        '<div class="weui-cell">'+
-            '<div class="weui-cell__hd">'+
-                '<label class="weui-label" for="">'+
-                    '剩余课时'+
-                '</label>'+
-            '</div>'+
-            '<div class="weui-cell__bd">'+
-                '<input class="weui-input" pattern="[0-9]*" type="number" value="24"/>'+
-            '</div>'+
-        '</div>'+
-    '</div>'
-    );
-  });
+var app = new Vue({
+    el: '#children',
+    data: {
+        children : [
+            { removable : false }
+        ]
+    },
+    methods: {
+        removeChild : function(index, child) {
+            app.children.splice(index, 1);
+        },
+        addChild: function() {
+            app.children.push({removable : true});
+        }
+    }
 });
