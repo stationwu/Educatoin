191bed05609dacc6eb240832e61eeef4e2275703
Mao, Weicheng <weicheng.mao@gmail.com>
2017-11-22 21:53:44 +0800

    update the message dialog with weui component.
    Change-Id: I701f883d11c912ec87b6ef1ed2a3b6301241fff8
    Signed-off-by: Mao, Weicheng <weicheng.mao@gmail.com>

--- src/main/resources/static/js/user_signup.js
+++ src/main/resources/static/js/user_signup.js
@@ -1,25 +1,25 @@
 var app = new Vue({
     el: '#signup',
     data: {
-        children : [
+        children: [
             {
                 name: "",
                 birthday: ""
             }
         ],
-        mobilePhone : "",
-        verificationCode : "",
+        mobilePhone: "",
+        verificationCode: "",
         address: "",
-        removable : false,
+        removable: false,
     },
     methods: {
-        removeChild : function(index, child) {
+        removeChild: function (index, child) {
             app.children.splice(index, 1);
             if (app.children.length == 1) {
                 app.removable = false;
             }
         },
-        addChild: function() {
+        addChild: function () {
             app.children.push({
                 name: "",
                 birthday: ""
@@ -28,64 +28,64 @@ var app = new Vue({
                 app.removable = true;
             }
         },
-        onClickGetVerificationCode : function() {
+        onClickGetVerificationCode: function () {
             console.log("get veri code clicked");
         },
-        onAddStudents: function(){
-        	var openCode   = $('#wxOpenCode').text();
-        	var customerid = $('#customerid').text();
-        	var students = [];
-        	var bHasError = false;
-        	
-            for(var i = 0; i < app.children.length; ++i) {
-            	var sChildName 	   = app.children[i].name.trim();
-            	var sChildBirthday = app.children[i].birthday.trim();
-            	
-            	if(sChildName === '' || sChildBirthday === ''){
-            		$.alert("请填写学员的姓名，生日信息","提示!");
-            		bHasError = true;
-            		break;
-            	} else {
-            		students.push({
-                        "childName"   : app.children[i].name,
-                        "birthday"    : app.children[i].birthday
+        onAddStudents: function () {
+            var openCode   = $('#wxOpenCode').text();
+            var customerid = $('#customerid').text();
+            var students   = [];
+            var bHasError  = false;
+
+            for (var i = 0; i < app.children.length; ++i) {
+                var sChildName     = app.children[i].name.trim();
+                var sChildBirthday = app.children[i].birthday.trim();
+
+                if (sChildName === '' || sChildBirthday === '') {
+                    $.alert("请填写学员的姓名，生日信息", "提示!");
+                    bHasError = true;
+                    break;
+                } else {
+                    students.push({
+                        "childName": app.children[i].name,
+                        "birthday": app.children[i].birthday
                     });
-            	}
+                }
             }
-            
-            if(!bHasError || students.length === 0) {
+
+            if (!bHasError || students.length === 0) {
                 $.ajax({
                     url: '/api/v1/Customer/AddChild',
                     contentType: 'application/json',
                     type: 'POST',
                     data: JSON.stringify(students),
-                }).done(function(response) {
+                }).done(function (response) {
                     window.location.href = "/user/center";
                 });
             }
         },
-        onSubmit: function() {
+        onSubmit: function () {
             var openCode = $('#wxOpenCode').text();
             var customer = {
-                "openCode"     : openCode,
-                "name"         : app.children[0].name + "家长",
-                "mobilePhone"  : app.mobilePhone,
-                "address"      : app.address,
-                "children"     : []
+                "openCode": openCode,
+                "name": app.children[0].name + "家长",
+                "mobilePhone": app.mobilePhone,
+                "address": app.address,
+                "children": []
             };
-            for(var i = 0; i < app.children.length; ++i) {
+            for (var i = 0; i < app.children.length; ++i) {
                 customer.children.push({
-                    "childName"   : app.children[i].name,
-                    "birthday"    : app.children[i].birthday
+                    "childName": app.children[i].name,
+                    "birthday": app.children[i].birthday
                 });
             }
-
+            
             $.ajax({
                 url: '/api/v1/Customer/SignUp',
                 contentType: 'application/json',
                 type: 'POST',
                 data: JSON.stringify(customer),
-            }).done(function(response) {
+            }).done(function (response) {
                 window.location.href = "/user/center";
             });
         },
