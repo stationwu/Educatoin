b32d7dd4960a3d2a1e35e40c3c53e08965438172
Guo, Yong Kang <guogin@outlook.com>
2017-12-08 23:08:35 +0800

    Lock at refund

--- src/main/java/com/edu/dao/PaymentRepository.java
+++ src/main/java/com/edu/dao/PaymentRepository.java
@@ -1,7 +1,15 @@
 package com.edu.dao;
 
 import com.edu.domain.Payment;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.persistence.LockModeType;
+
 public interface PaymentRepository extends CrudRepository<Payment, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select * from Payment where id = ?1")
+    Payment findOneForUpdate(Long id);
 }
